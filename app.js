97e63f11ae8419189bf3a6c3191e7812e483b899
Crystal Goodwin <sgcg03@mac.com>
2023-02-07 15:31:15 -0500

    Added Javascript functionality for light/dark mode

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 // !TOGGLE ICONS //
 
+const themeToggleButton = document.querySelector('.theme-toggle-button');
+const bodyElement = document.body;
+const currentTheme = localStorage.getItem('darkTheme');
+
+if (currentTheme) {
+   bodyElement.classList.add('dark-theme');
+};
+
+const toggleTheme = () => {
+   bodyElement.classList.toggle('dark-theme');
+
+   if (bodyElement.classList.contains('dark-theme')) {
+      localStorage.setItem('darkTheme', 'active');
+   } else {
+      localStorage.removeItem('darkTheme');
+   }
+};
+
+themeToggleButton.addEventListener('click', toggleTheme);
