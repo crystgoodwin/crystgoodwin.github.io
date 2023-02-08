95dda862935170d3031e6b140774d9ad29591a7f
Crystal Goodwin <sgcg03@mac.com>
2023-02-08 14:02:40 -0500

    Added scroll animation to JavaScript file

--- app.js
+++ app.js
@@ -19,3 +19,18 @@ const toggleTheme = () => {
 };
 
 themeToggleButton.addEventListener('click', toggleTheme);
+
+//!Scroll Reveal
+
+const sr = ScrollReveal({
+   distance: '50px',
+   duration: 1500,
+   easing: 'cubic-bezier(0.68, -0.55, 0.265, 1.55)'
+});
+
+sr.reveal('.main-title', { origin: 'top' });
+sr.reveal('.scroll-reveal-left', { origin: 'left' });
+sr.reveal('.scroll-reveal-right', { origin: 'right' });
+sr.reveal('.tech-stack-item', { interval: 250 });
+sr.reveal('.tech-stack-item', { origin: 'top', interval: 250 });
+sr.reveal('.portfolio-card', { interval: 500 });
